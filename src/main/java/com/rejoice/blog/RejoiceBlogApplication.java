1e0f0e2ba40fb6286fe767340872fcfb635d22f9
zhangjiongzhan <948870341@qq.com>
2018-05-19 22:58:03 +0800

    1

--- src/main/java/com/rejoice/blog/RejoiceBlogApplication.java
+++ src/main/java/com/rejoice/blog/RejoiceBlogApplication.java
@@ -1,18 +1,33 @@
 package com.rejoice.blog;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import javax.annotation.PostConstruct;
 
-@SpringBootApplication
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication(exclude=SecurityAutoConfiguration.class)
 @MapperScan(basePackages="com.rejoice.blog.mapper")
 public class RejoiceBlogApplication {
 
 	public static void main(String[] args) {
+		System.err.println("2332");
 		new SpringApplicationBuilder(RejoiceBlogApplication.class)
 		.properties(
 				"spring.config.name:application,constant,task,enabled,boot-admin,jdbc",
 				"spring.config.location:classpath:/,classpath:/config/,file:./config/")
 		.build().run(args);
+	}
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@PostConstruct
+	public void init() {
+		System.err.println(passwordEncoder.encode("user"));
 	}
 }
