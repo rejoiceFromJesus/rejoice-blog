2126d1f591ce34386a31c990856ca6f0561b8c87
zhangjiongzhan <948870341@qq.com>
2018-05-20 23:36:53 +0800

    customer redirectUrl when login successful

--- src/main/java/com/rejoice/blog/security/support/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/rejoice/blog/security/support/MyAuthenticationSuccessHandler.java
@@ -10,7 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import com.rejoice.blog.common.util.JsonUtil;
@@ -30,6 +34,26 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
 			writer.write(JsonUtil.toJson(authentication));
 			return;
 		}
+		 String redirectUrl = null;
+		/*SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+        redirectUrl = (savedRequest == null) ? "" : savedRequest.getRedirectUrl();
+        System.err.println(redirectUrl);
+        if (!redirectUrl.isEmpty()) {
+            response.sendRedirect(redirectUrl);
+            return;
+        }*/
+		// 获取登录用户信息
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        for (GrantedAuthority authority : auth.getAuthorities()) {
+            String role = authority.getAuthority(); // 用户的权限
+            // 不同的用户访问不同的页面
+            if ("ROLE_ADMIN".equals(role)) {
+                redirectUrl = "/page/admin/index.html";
+            } else {
+                redirectUrl = "/";
+            }
+        }
+        response.sendRedirect(redirectUrl);
 		super.onAuthenticationSuccess(request, response, authentication);
 	}
 
