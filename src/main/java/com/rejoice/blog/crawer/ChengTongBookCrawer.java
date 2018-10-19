e12ac15ef5be81d98c837bee2b873054f198cc99
zhangjiongzhan <Administrator@JEKBLFE0O8QU6MT>
2018-10-19 20:27:52 +0800

    handle e

--- src/main/java/com/rejoice/blog/crawer/ChengTongBookCrawer.java
+++ src/main/java/com/rejoice/blog/crawer/ChengTongBookCrawer.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Component;
 
 import com.rejoice.blog.common.util.JsonUtil;
 
+import groovyjarjarasm.asm.tree.TryCatchBlockNode;
+
 @Component
 public class ChengTongBookCrawer extends BookCrawer{
 	
@@ -30,30 +32,40 @@ public class ChengTongBookCrawer extends BookCrawer{
 					Document level1 = Jsoup.connect(url).get();
 					Elements level1A = level1.select(".widget-body  a");
 					for (Element element : level1A) {
-						String href = element.absUrl("href");
-						Document level2 = Jsoup.connect(href).get();
-						Elements resourceList = level2.select("#resource-list a");
-						for (Element resource : resourceList) {
-							if (resource.text().endsWith(".pdf")) {
-								String downloadPageUrl = resource.absUrl("href");
-								String[] uidFid = downloadPageUrl.split("/")[4].split("-");
-								Document document = Jsoup.connect(downloadPageUrl).get();
-								String downloadFun = document.select(".download-box a").first().attr("onclick");
-								String[] downloadPrams = downloadFun.substring(10, downloadFun.length()-2).replace("'", "").replaceAll(" ", "").split(",");
-								String getDownloadUrl = GET_DOWNLOAD_URL.replaceAll("\\$\\{uid\\}", uidFid[0])
-										.replaceAll("\\$\\{fid\\}", uidFid[1])
-										.replaceAll("\\$\\{file_chk\\}", downloadPrams[2]);
-								HttpHeaders headers = new HttpHeaders();
-								headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-								HttpEntity<Object> entity = new HttpEntity<>(headers);
-								Map data = restTemplate.exchange(getDownloadUrl, HttpMethod.GET, entity, Map.class).getBody();
-								if(data.get("code").equals(200)) {
-									this.downloadBook(data.get("downurl").toString(), downloadPageUrl, resource.text());
-								}else {
-									LOGGER.warn("get downurl failed:{}", JsonUtil.toJson(data));
+						try {
+							String href = element.absUrl("href");
+							Document level2 = Jsoup.connect(href).get();
+							Elements resourceList = level2.select("#resource-list a");
+							for (Element resource : resourceList) {
+								if (resource.text().endsWith(".pdf")) {
+									try {
+										String downloadPageUrl = resource.absUrl("href");
+										String[] uidFid = downloadPageUrl.split("/")[4].split("-");
+										Document document = Jsoup.connect(downloadPageUrl).get();
+										String downloadFun = document.select(".download-box a").first().attr("onclick");
+										String[] downloadPrams = downloadFun.substring(10, downloadFun.length()-2).replace("'", "").replaceAll(" ", "").split(",");
+										String getDownloadUrl = GET_DOWNLOAD_URL.replaceAll("\\$\\{uid\\}", uidFid[0])
+												.replaceAll("\\$\\{fid\\}", uidFid[1])
+												.replaceAll("\\$\\{file_chk\\}", downloadPrams[2]);
+										HttpHeaders headers = new HttpHeaders();
+										headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+										HttpEntity<Object> entity = new HttpEntity<>(headers);
+										Map data = restTemplate.exchange(getDownloadUrl, HttpMethod.GET, entity, Map.class).getBody();
+										if(data.get("code").equals(200)) {
+											this.downloadBook(data.get("downurl").toString(), downloadPageUrl, resource.text());
+										}else {
+											LOGGER.warn("get downurl failed:{}", JsonUtil.toJson(data));
+										}
+									} catch (Exception e) {
+										LOGGER.warn("crawer books failed :",e);
+									}
+									
 								}
 							}
+						} catch (Exception e) {
+							LOGGER.warn("crawer books failed :",e);
 						}
+						
 					}
 				} catch (Exception e) {
 					LOGGER.warn("crawer cheng tong books failed:{}", url, e);
